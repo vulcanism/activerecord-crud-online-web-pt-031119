8960789c0a2f706601c5b1266b53ef65d5feea35
Savannah C <sgcorrell@gmail.com>
2019-05-30 18:42:13 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,6 +30,7 @@ def can_be_created_in_a_block(args = {title: "Home Alone", release_date: 1990})
   
   Movie.create do |m|
     m.title = args[:title]
+    m.release_date = args[:release_date]
   end
 end
 
